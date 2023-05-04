656ca1c210962fb9cc9118a7421dd716388eb9d0
surya21112000 <surya@market-intellect.com>
2023-05-04 16:02:27 +0530

    git deployPostws

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -47,6 +47,10 @@ export const Skills = () => {
                             </div>
                             <div className="item">
                                 <img src={meter3} alt="Image" />
+                                <h5>React Native</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter3} alt="Image" />
                                 <h5>Node js</h5>
                             </div>
                             <div className="item">
